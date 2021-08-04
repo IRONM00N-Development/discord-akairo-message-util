62c36e6ad670802dfe8b2a130c95af20b5e28ca7
IRONM00N <64110067+IRONM00N@users.noreply.github.com>
2021-08-04 19:17:05 -0400

    initial commit

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+declare module 'discord-akairo' {
+	import { CommandUtil } from 'discord-akairo';
+
+	module 'discord.js' {
+		export interface Message {
+			/**
+			 * Extra properties applied to the Discord.js message object.
+			 * Utilities for command responding.
+			 * Available on all messages after 'all' inhibitors and built-in inhibitors (bot, client).
+			 * Not all properties of the util are available, depending on the input.
+			 * */
+			util?: CommandUtil;
+		}
+	}
+}
